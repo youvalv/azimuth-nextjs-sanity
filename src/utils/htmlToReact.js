33e144e53bd7306b958c2d12f1765048f758b5bf
Simon Hanukaev <semenh@gmail.com>
2021-02-19 10:43:00 +0200

    added back ScriptTag by adding babel-runtime

--- src/utils/htmlToReact.js
+++ src/utils/htmlToReact.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactHtmlParser, { convertNodeToElement } from 'react-html-parser';
-// import ScriptTag from 'react-script-tag';
-// import _ from 'lodash';
+import ScriptTag from 'react-script-tag';
+import _ from 'lodash';
 
 export default function htmlToReact(html) {
     if (!html) {
         return null;
     }
     return ReactHtmlParser(html, {
-/*
         transform: (node, index) => {
             if (node.type === 'script') {
                 if (!_.isEmpty(node.children)) {
@@ -22,6 +21,5 @@ export default function htmlToReact(html) {
                 }
             }
         }
-*/
     });
 };
